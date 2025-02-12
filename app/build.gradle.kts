c5cf716ac48b25cec6192a510808821fad86e980
RobertoMay <7630@itescam.edu.mx>
2025-02-11 21:46:43 -0600

    carousel of images

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,6 +53,10 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+
+    implementation(libs.imageslideshow)
+
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/example/albergue_android/ui/home/HomeActivity.kt
+++ app/src/main/java/com/example/albergue_android/ui/home/HomeActivity.kt
@@ -7,12 +7,27 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.albergue_android.R
 import androidx.activity.ComponentActivity
+import com.denzcoskun.imageslider.ImageSlider
+import com.denzcoskun.imageslider.models.SlideModel
+import com.denzcoskun.imageslider.constants.ScaleTypes
 
 class HomeActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 //        enableEdgeToEdge()
         setContentView(R.layout.activity_home)
 
+        val imageSlider = findViewById<ImageSlider>(R.id.image_slider);
+        val imageList = ArrayList<SlideModel>();
+
+        imageList.add(SlideModel(R.drawable.i1, scaleType = ScaleTypes.CENTER_CROP));
+        imageList.add(SlideModel(R.drawable.i2, scaleType = ScaleTypes.CENTER_CROP));
+        imageList.add(SlideModel(R.drawable.i3, scaleType = ScaleTypes.CENTER_CROP));
+
+//        imageList.add(SlideModel("https://bit.ly/2YoJ77H", "The animal population decreased by 58 percent in 42 years."))
+//        imageList.add(SlideModel("https://bit.ly/2BteuF2", "Elephants and tigers may become extinct."))
+//        imageList.add(SlideModel("https://bit.ly/3fLJf72", "And people do that."))
+        imageSlider.setImageList(imageList)
     }
 }
