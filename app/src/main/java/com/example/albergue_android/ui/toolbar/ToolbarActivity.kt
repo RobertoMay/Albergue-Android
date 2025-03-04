c021bcad68070c1607cdaf43088b3e999324125f
CrismarJC <7612@itescam.edu.mx>
2025-03-03 22:57:44 -0600

    Recuperando cambios desde stash y trabajo finalizado en el toolbar

--- app/src/main/java/com/example/albergue_android/ui/toolbar/ToolbarActivity.kt
+++ app/src/main/java/com/example/albergue_android/ui/toolbar/ToolbarActivity.kt
@@ -12,7 +12,7 @@ class ToolbarActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_toolbar)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.toolbar)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
